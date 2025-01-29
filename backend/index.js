ceb2707dd91d7d146c33c178c1a2c83bd9b5a0fa
Branden Taylor <brataylor@microsoft.com>
2025-01-28 19:33:38 -0800

    Updating API endpoints

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,8 @@ app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
 
-app.post('/create-user-profile', (req, res) => {
-  res.json(req.body);
-});
-
 app.post('/createProfile', (req, res) => {
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 app.put('/updateProfile', (req, res) => {
@@ -25,30 +21,43 @@ app.put('/updatePreferences', (req, res) => {
 });
 
 app.get('/getRecommendations', (req, res) => {
+  const { uid } = req.query;
+  // Process the UID here
   res.json({ recommendations: ['person1', 'person2', 'person3'] });
 });
 
 app.post('/sendMessage', (req, res) => {
+  const { senderUid, receiverUid, message } = req.body;
+  // Process the message here
   res.sendStatus(200);
 });
 
 app.get('/getLikes', (req, res) => {
+  const { uid } = req.query;
+  // Process the UID here
   res.json({ likes: ['user1', 'user2', 'user3'] });
 });
 
 app.post('/swipe', (req, res) => {
+  const { swiperUid, swipeyUid, direction } = req.body;
+  // Process the swipe here
   res.json({ match: true });
 });
 
 app.get('/getMatches', (req, res) => {
+  const { uid } = req.query;
+  // Process the UID here
   res.json({ matches: ['match1', 'match2', 'match3'] });
 });
 
 app.get('/getMessages', (req, res) => {
+  const { uid, receiverUid } = req.query;
+  // Process the UIDs here
   res.json({ messages: ['message1', 'message2', 'message3'] });
 });
 
 app.post('/blockUser', (req, res) => {
+    const { uid, uidToBlock } = req.query;
   res.sendStatus(200);
 });
 
